5921fdd904853414991472344fae1b1e9aa9cf4f
ThanusanS <thanusan0411@gmail.com>
2026-02-16 17:34:02 +0530

    first commit

---
+++ Function_01.py
@@ -0,0 +1,23 @@
+def greet(username):
+    print(f"Hello Welcome {username}")
+
+
+
+greet("abc")
+greet("bcd")
+
+
+
+
+def fname(firstName,LastName):
+    fullName= firstName+LastName
+    return fullName
+
+
+
+name=fname("abc", "cde")
+
+print(name)
+
+
+
